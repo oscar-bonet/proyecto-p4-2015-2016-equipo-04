43e6df369f0315970f2440e7f3e52e4f8be09ab3
Oscar Bonet <oscar.bonet@opendeusto.es>
2016-06-01 19:19:02 +0200

    INSERT INTO (con errores)
    he añadido la funcionalidad de insert into, leyendo el ultimo libro del fichero. Hay algunos errores, pero tiene buena pinta :)

--- DBConnector.h
+++ DBConnector.h
@@ -78,4 +78,73 @@ public:
 		delete[] datos;
 		return SQLITE_OK;
 	}
+
+	int agregarLibro(sqlite3 *db){
+		int total;
+		std::ifstream f("libros.txt");
+		std::string line;
+		for (total = 1; std::getline(f, line); total++)
+		    ;
+
+	//HASTA AQUÍ DETECTAMOS CUANTAS FILAS HAY EN EL FICHERO PARA AHORA AGREGAR SOLO EL ÚLTIMO 
+
+		FILE* f;
+	    char leer[100];
+	    char linea[100];
+	    int counter =1;
+	    int cont = 0;
+	    int aux = 1;
+	    Libro *lib = (Libro*) malloc (sizeof(Libro));
+
+		f = fopen("libros.txt", "r");
+
+	    if (f==NULL){
+	    	printf("Error al abrir el fichero\n");
+
+	    }else{
+			
+	    	while (fgets(linea, 100, f)) {
+	    		if (total-8 == cont)
+	    		{
+		    		clear_if_needed(linea);
+					sscanf(linea, "%[^\n]", &leer);
+					switch(aux){
+						case 1: lib->isbn = leer; break;
+						case 2: lib->titulo = leer; break;
+						case 3: lib->autor = leer; break;
+						case 4: lib->genero = leer; break;
+						case 5: lib->precio = leer; break;
+						case 6: lib->desc = leer; break;
+						case 7: lib->anyo = leer; break;
+						case 8: lib->editorial = leer; break;
+					}
+					aux++;
+					total++;
+	    		} cont++;
+	    		
+			}
+		}
+		fclose(f);
+
+	//HEMOS LEIDO LOS ATRIBUTOS DEL LIBRO A GUARDAR, AHORA VAMOS A METERLO EN LA BD
+
+		std::stringstream strm;
+		strm << "insert into libro values('" << lib->isbn << ",'" << lib->titulo << "','" << lib->autor << "','" << lib->genero
+			<< "','" << lib->precio << "','" << lib->desc << "','" << lib->anyo << "','" << lib->editorial << "')";
+
+		char *str = &s[0];
+		sqlite3_stmt * statement;
+		int result;
+		char *query = str;
+	    {
+	        if(sqlite3_prepare(dbfile,query,-1,&statement,0)==SQLITE_OK)
+	        {
+	            int res=sqlite3_step(statement);
+	            result=res;
+	            sqlite3_finalize(statement);
+	        }
+	        return result;
+	    }
+	    return 0;
+	}
 };
